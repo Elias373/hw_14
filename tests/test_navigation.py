df3cc89b064fc6ffab9adf7459e3e942bac3effa
Illia Karcheuski <your.email@example.com>
2025-12-06 12:35:51 +0300

    allure upd

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,27 +1,25 @@
 import allure
 from selene import browser, have
-from pages.main_page import MainPage
 
 
-@allure.label("epic", "Navigation")
-@allure.label("story", "Menu")
-@allure.title("Menu")
-def test_navigation(authorized_user):
-    with allure.step("Perform Login"):
-        main = authorized_user
-    with allure.step("Open about page via menu"):
-        main.open_about_page()
-    with allure.step("Verify about page opened"):
-        browser.should(have.url_containing('saucelabs.com'))
 
+@allure.epic("Navigation")
+class TestNavigation:
 
-@allure.label("epic", "Navigation")
-@allure.label("story", "Product Filtering")
-@allure.title("Product Filtering")
-def test_product_filtering(authorized_user):
-    with allure.step("Perform Login"):
-        main = authorized_user
-    with allure.step("Apply price filter low to high"):
-        browser.element('.product_sort_container').element('option[value="lohi"]').click()
-    with allure.step("Verify filter is applied"):
-        browser.element('.active_option').should(have.text('Price (low to high)'))
+    @allure.title("Menu")
+    def test_navigation(self, authorized_user):
+        with allure.step("Perform Login"):
+            main = authorized_user
+        with allure.step("Open about page via menu"):
+            main.open_about_page()
+        with allure.step("Verify about page opened"):
+            browser.should(have.url_containing('saucelabs.com'))
+
+    @allure.title("Product Filtering")
+    def test_product_filtering(self, authorized_user):
+        with allure.step("Perform Login"):
+            main = authorized_user
+        with allure.step("Apply price filter low to high"):
+            browser.element('.product_sort_container').element('option[value="lohi"]').click()
+        with allure.step("Verify filter is applied"):
+            browser.element('.active_option').should(have.text('Price (low to high)'))
